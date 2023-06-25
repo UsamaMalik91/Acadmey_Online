92bee206348fa1f0313b6ca21112839db9a4a5d9
Usama_Malik <usamamm510@gmail.com>
2023-06-25 14:06:36 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar=()=>{
     }
     return(
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>ACADEMY_ONLINE.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>ACADEMY<Typed strings={['...']} typeSpeed={100} backSpeed={130} loop showCursor='none' /></h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>courses</li>
@@ -20,7 +20,7 @@ const Navbar=()=>{
         {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[85%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500': 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>ACADEMY_ONLINE.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>ACADEMY<Typed strings={['...']} typeSpeed={100} backSpeed={130} loop showCursor='none' /></h1>
             <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-600'>Home</li>
             <li className='p-4 border-b border-gray-600'>Courses</li>
