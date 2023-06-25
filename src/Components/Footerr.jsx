25f06ed521bd09d6c710d9f1abc1ccf9a6b5ac86
Usama_Malik <usamamm510@gmail.com>
2023-06-25 14:30:38 +0500

    Update Footerr.jsx

--- src/Components/Footerr.jsx
+++ src/Components/Footerr.jsx
@@ -41,7 +41,7 @@ const Footerr = () => {
             <li className='py-2 text-sm'>Pricing</li>
             <li className='py-2 text-sm'>Documentation</li>
             <li className='py-2 text-sm'>Guides</li>
-            <li className='py-2 text-sm'>Rules & Regulation</li>
+            <li className='py-2 text-sm'>Rules</li>
         </ul>
     </div>
     <div>
@@ -49,7 +49,7 @@ const Footerr = () => {
         <ul>
             <li className='py-2 text-sm'>Members</li>
             <li className='py-2 text-sm'>Students</li>
-            <li className='py-2 text-sm'>Achievements</li>
+            <li className='py-2 text-sm'>Awards</li>
             <li className='py-2 text-sm'>Careers</li>
         </ul>
     </div>
