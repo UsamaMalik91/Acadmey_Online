34988112213af11c0024d0ebd9663e4d2ce901c6
Usama_Malik <usamamm510@gmail.com>
2023-06-25 10:52:08 +0500

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ return (
         The Path Of Success Is Here.
         </p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-        Grow with data.
+        Grow with Us.
         </h1>
         <div className='flex justify-center items-center'>
         <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
